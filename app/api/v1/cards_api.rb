c8a2893dc6bd2d3d6e2f2c98af4fa7290b817516
niexinxin <18612765436@163.com>
2018-08-19 19:47:02 +0800

    a

--- app/api/v1/cards_api.rb
+++ app/api/v1/cards_api.rb
@@ -131,6 +131,10 @@ module V1
       end
       post "cengke" do
         share = Share.where(id: params[:share_id], source_type: "Card").first
+        return return_success({
+          max_share_count: 20,
+          current_share_count: Cengke.where(source_user_id: share.user_id,card: card).count
+        }) if current_user.id == share.user_id
         return return_fail('该分享不存在') if share.blank?
         card = Card.where(id: share.source_id).first
         return return_fail('该卡片不存在') if card.blank?
